dc64b2f9edb0072e5f5c6251d0faa513cba5c718
mikhail-mfib <48675855+mikhail-mfib@users.noreply.github.com>
2019-06-27 22:25:51 +0300

    First hw
    Created new project

---
+++ script.js
@@ -0,0 +1,9 @@
+let money = 300;
+let income = 110;
+let addExpenses = 15;
+let deposit = 30;
+let mission = 'Выучить иностранный язык';
+let period = 'month';
+
+alert('Задача на завтра: ' + mission);
+console.log('Первое сообщение в консоли');
