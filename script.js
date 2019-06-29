72d6949f2fb2464998afe0017302b1e354d03c53
Mikhail Sennikov <mikhail-mfib@yandex.ru>
2019-06-29 17:12:54 +0300

    Второй урок, основная чатьс

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 130000;
+let income = 'ценные бумаги';
+let addExpenses = 'Питание, Недвижимость, Обучение';
+let deposit = true;
+let mission = 10000000;
+let period = 11;
+let budgetDay = money / 30;
 
-alert('Первый алерт');
-console.log('Первое сообщение в консоли');
+console.log(typeof money, typeof income, typeof deposit);
+console.log(income.length);
+console.log(`Период ${period} месяцев. Цель заработать ${mission} рублей/долларов/гривен/юани`);
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(`Доход за месяц: ${budgetDay}. Остаток: ${money % 30}`);
